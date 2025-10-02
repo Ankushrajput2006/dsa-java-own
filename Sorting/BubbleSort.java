abbf48d91fae955b26a54d7657ddec1d874736a8
Ankush Singh <ankushsingh022006@gmail.com>
2025-10-02 20:07:29 +0530

    uploading

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -12,14 +12,18 @@ public class BubbleSort {
     static void bubble(int[] arr){
         int n = arr.length;
         for (int i = 0; i < n; i++) {
+            boolean swapped = false;
             for(int j=1;j<n-i;j++){
                 if(arr[j]<arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
-                }
+                    swapped = true;
+                }  
             }
-            
+        if(!swapped){
+            break;
+        }   
         }
     }
     
