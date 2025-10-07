1ad5d68e56df30a00bf1d43f7d6f947db825b889
Ankush Singh <ankushsingh022006@gmail.com>
2025-10-07 21:01:46 +0530

    uploading

--- Recursion/fibonacie.java
+++ Recursion/fibonacie.java
@@ -2,11 +2,10 @@
 
 public class fibonacie {
     public static void main(String[] args) {
-        int n = 7;
-        System.out.println(fibo(n));;
+        System.out.println(fibo(6));;
     }
     static int fibo(int n){
-        if(n>2){
+        if(n<2){
             return n;
         }
         return fibo(n-1)+fibo(n-2);
